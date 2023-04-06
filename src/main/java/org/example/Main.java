b043274a14c44711998e8bdaa759beefe85fe896
Amir141285 <amir1412@mail.ru>
2023-04-06 22:47:50 +0600

    Created new project for able git workshop

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        mainMethodMessageAble();
+        mainMethodSh0winfo();
+    }
+    private static void mainMethodMessageAble() {
+        System.out.println("Hello able QA course\n");
+    }
+
+    private static void mainMethodSh0winfo() {
+        System.out.println("Lets learn Git");
+    }
+}
+
